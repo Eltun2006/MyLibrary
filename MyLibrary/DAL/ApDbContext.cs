4687c0537a3da04f8411c22de9a7463a8cccfd70
Eltun2006 <eltun.memmedli.06@gmail.com>
2025-10-25 23:32:20 +0400

    Finished

--- MyLibrary/DAL/ApDbContext.cs
+++ MyLibrary/DAL/ApDbContext.cs
@@ -4,16 +4,15 @@ using MyLibrary.Models;
 
 namespace MyLibrary.DAL
 {
-    public class ApDbContext : DbContext
+    // ✅ BURA BAX: IDataProtectionKeyContext əlavə etməlisən
+    public class ApDbContext : DbContext, IDataProtectionKeyContext
     {
         public ApDbContext(DbContextOptions<ApDbContext> options) : base(options)
         {
         }
 
-        public DbSet<User>Users { get; set; }
-
-        public DbSet<Book>Books { get; set; }
-
+        public DbSet<User> Users { get; set; }
+        public DbSet<Book> Books { get; set; }
         public DbSet<DataProtectionKey> DataProtectionKeys { get; set; }
     }
 }
